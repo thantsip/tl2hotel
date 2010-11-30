dc2dca92a3c8774e66b67dbaa77fdd8b7e0dc3f8
alexander.iordanidis@gmail.com <alexander.iordanidis@gmail.com@fb7cbe1a-da42-76e9-2caa-fedf319af631>
2010-11-30 17:29:01 +0000

    ta evala pali gt den vlepan oi alloi

---
+++ DEV/Ιορδανίδης Αλέξανδρος/Class reservationManagement/reservationManagement.h
@@ -0,0 +1,66 @@
+#ifndef RESERVATIONMANAGEMENT_H
+#define RESERVATIONMANAGEMENT_H
+#include "sqlMechanism.h"
+#include "customer.h"
+#include "room.h"
+#include <QMessageBox>
+#include <QString>
+#include <QChar>
+
+/**
+ *Reservation Management Class
+ *Checks if selected room is free and adds reservation to the database,or cancels existing reservation or makes checkout of reservation
+ *@param reservationId is an integer identifier number
+ *@see roomReservation()
+ *@makes the reservation of the selected room
+ */
+class ReservationManagement
+{
+private:
+    Customer customer;
+    Room room;
+    SQLMechanism sqlMechanism;
+    int reservationId;
+    QString dateFrom;
+    QString dateTo;
+public:
+       /**
+        *Default Constructors
+        */
+       ReservationManagement();
+
+       /** Copy,set,get Constructors
+        *
+        *@param reservationId1 is a copy variable
+        *@param dateFrom1 is a copy variable
+        *@param dateTo1 is a copy variable
+        */
+       ReservationManagement(int reservationId1,QString dateFrom1,QString dateTo1);
+	   
+       int getReservationId(); 
+       void setReservationId(int reservationId1);
+	   
+       QString getDateFrom();
+       void setDateFrom(QString dateFrom1);
+	   
+       QString getDateTo();
+       void setDateTo(QString dateTo1);
+
+       /**
+         *function that adds a new reservation to RoomsReservation table
+         */
+       void roomReservation(QString DateFrom,QString DateTo,Room room,Customer customer);
+       /**
+         *function that checks the data given by the user
+         */
+       bool checkInData(Room room,Customer customer);
+       //roomCancelation();
+       //roomCheckout();
+
+};
+
+
+
+
+
+#endif // RESERVATIONMANAGEMENT_H
