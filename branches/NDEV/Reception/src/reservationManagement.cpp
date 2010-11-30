7f671dfead494b3e07fc30b0b7aab79d536b7151
alexander.iordanidis <alexander.iordanidis@gmail.com>
2010-11-30 21:55:41 +0000

    added class ReservationManagement changed Changelog.txt

--- branches/NDEV/Reception/src/reservationManagement.cpp
+++ branches/NDEV/Reception/src/reservationManagement.cpp
@@ -1,8 +1,9 @@
 #include "reservationManagement.h"
 
 
- /** Default Constructors
-*/
+ /**
+   *Default Constructors
+   */
 ReservationManagement::ReservationManagement()
 {
     reservationId=0;
@@ -12,14 +13,14 @@ ReservationManagement::ReservationManagement()
 
 
 
-/** Copy,set,get Constructors
-       
-*/
+/**
+  *Copy,set,get Constructors
+  */
 ReservationManagement::ReservationManagement(int reservationId1,QString dateFrom1,QString dateTo1)
 {
-    reservationId=reservationId1;
-        dateFrom = dateFrom1;
-        dateTo = dateTo1;
+    reservationId = reservationId1;
+    dateFrom = dateFrom1;
+    dateTo = dateTo1;
 }
 
 
@@ -29,19 +30,16 @@ int ReservationManagement::getReservationId()
 }
 
 
-
 QString ReservationManagement::getDateFrom()
 {
     return dateFrom;
 }
 
 
-
 QString ReservationManagement::getDateTo()
 {
     return dateTo;
 }
-
 
 
 void ReservationManagement::setReservationId(int reservationId1 )
@@ -60,21 +58,79 @@ void ReservationManagement::setDateTo(QString dateTo1 )
      dateTo = dateTo1;
 }
 
-/** Room Reservation Function
-	*makes the reservation of the selected room
-        *
-*/
+/**
+  *Room Reservation Function
+  *makes the reservation of the selected room
+  */
 
-void ReservationManagement::roomReservation(QString DateFrom,QString DateTo,int roomId,int CustomerId)
+void ReservationManagement::roomReservation(QString DateFrom,QString DateTo,Room room,Customer customer)
 {
-    if( room.getFree() )
+    /**
+      *check if there is any available room
+      *if there is insert a reservation
+      */
+    if(!room.getFree())
     {
-        sqlMechanism.execQuery("insert into RoomsReservation (DateFrom,DateTo,fkCustomerId,fkRoomId) values('"+DateFrom+"','"+DateTo+"','"+QString("%1").arg(roomId)+"','"+QString("%1").arg(CustomerId)+"') ");
-    }
-    else
-    {
+        /**
+          *message that informs that there is none available room
+          */
         QMessageBox msgBox;
         msgBox.setText("Reservation not made.Room is not free at the moment.");
         msgBox.exec();
     }
+    /**
+      *check if the data given by the user are correct
+      */
+    if(ReservationManagement::checkInData(room,customer))
+    {
+        /**
+          *insert the data into table RoomsResevation
+          */
+        sqlMechanism.execQuery("insert into RoomsReservation (DateFrom,DateTo,fkCustomerId,fkRoomId) values('"+DateFrom+"','"+DateTo+"','"+QString("%1").arg(customer.getId())+"','"+QString("%1").arg(room.getRoomNumber())+"') ");
+    }
+}
+/**
+  *a function that checks if the data given by the user are correct
+  *if not a message appears that informs for an error
+  */
+bool ReservationManagement::checkInData(Room room, Customer customer)
+{
+
+    int rNum=room.getRoomNumber();
+    QString cId=customer.getId();
+    QChar c;
+    int error = 0;
+    bool ret = false;
+
+    /**
+      *check that the customerId has no spaces or symbols
+      */
+    foreach(c,cId)
+    {
+        if(!c.isLetterOrNumber())
+        {
+           error++;
+        }
+    }
+    if(error != 0)
+    {
+      QMessageBox::critical(0,"Input Data Error","Check customer id");
+      ret = false;
+    }
+
+    if(rNum==0)
+        {
+            QMessageBox::critical(0,"Input Data Error","Check the Room Number");
+            ret = false;
+        }
+
+    else
+        ret = true;
+
+    /**
+      *@return true data is correct
+      *@return false if data is incorrect
+      */
+    return ret;
+
 }
